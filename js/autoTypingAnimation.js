3c2918464fbae966b03dc5776ab90620d815540a
Aayush Gupta <aayushgupta0403@gmail.com>
2023-07-21 00:33:09 +0530

    Update autoTypingAnimation.js

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<div>Hi, This is AAYUSH GUPTA</div> <span>I am B.Tech undergrad from Veer Surendra Sai University Of Technology in the branch of Information Technology ,currently i am in my prefinal year.<br><br> I am a Full-Stack Developer and also a passionate coder. i have a good knowledge of programming as well as web development,i also have done few projects as an intern and now looking for some great further oppertunities. <br><br> please have a look on my skills and projects so that for work related quieries you can hire me!!</span> "
+        "<div>Hi, This is AAYUSH GUPTA</div> <span>I am B.Tech undergrad from Veer Surendra Sai University Of Technology in the branch of Information Technology ,currently i am in my Final year.<br><br> I am a Full-Stack Developer and also a passionate coder. i have a good knowledge of programming as well as web development,i also have done few projects as an intern and now looking for some further oppertunities. <br><br> My skills and projects  are mentioned below please have a look so that for work related quieries you can hire me!!</span> "
       ],
       typeSpeed: 7,
       contentType: 'html'
